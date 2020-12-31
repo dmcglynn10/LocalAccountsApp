66b8781031f1c01ca8acfa068217c0e387c871ed
Daniel McGlynn <Daniel.McGlynn@domain.clickenergyni.com>
2020-12-31 16:54:16 +0000

    Add project files.

---
+++ LocalAccountsApp/Controllers/SchoolController.cs
@@ -0,0 +1,36 @@
+﻿using LocalAccountsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LocalAccountsApp.Controllers
+{
+    public class SchoolController : ApiController
+    {
+        public IHttpActionResult GetAllStudents()
+        {
+            IList<StudentViewModel> students = null;
+
+            using (var ctx = new SchoolDBEntities())
+            {
+                students = ctx.People.Include("StudentGrade")
+                            .Select(s => new StudentViewModel()
+                            {
+                                Id = s.PersonID,
+                                FirstName = s.FirstName,
+                                LastName = s.LastName
+                            }).ToList<StudentViewModel>();
+            }
+
+            if (students.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(students);
+        }
+    }
+}
---
+++ LocalAccountsApp/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using LocalAccountsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LocalAccountsApp.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            IEnumerable<StudentViewModel> students = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:44305/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("school");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<StudentViewModel>>();
+                    readTask.Wait();
+
+                    students = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    students = Enumerable.Empty<StudentViewModel>();
+
+                    //ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(students);
+        }      
+    }
+}
