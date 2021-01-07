bcf220f2adaa75ab7f175135c67e6b50e1e87b70
Daniel McGlynn <Daniel.McGlynn@domain.clickenergyni.com>
2021-01-07 21:16:08 +0000

    GetToken added to StudentController - DMG - 07/01/2021

--- LocalAccountsApp/Controllers/StudentController.cs
+++ LocalAccountsApp/Controllers/StudentController.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace LocalAccountsApp.Controllers
 {
@@ -42,10 +46,39 @@ namespace LocalAccountsApp.Controllers
             }
             return View(students);
         } 
-        
-        public ActionResult GetToken()
-        {
 
+        public async Task<ActionResult> GetToken()
+        {
+            JObject tokenBased = new JObject();
+            
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Clear();
+                client.BaseAddress = new Uri("https://localhost:44305/");
+                string body = "grant_type=password&username=daniel@email&password=Password1!";
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "/Token");
+                request.Content = new StringContent(body, Encoding.UTF8, "application/x-www-form-urlencoded");
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                var responseMessage = await client.SendAsync(request);
+                if(responseMessage.IsSuccessStatusCode)
+                {
+                    var resultMessage = responseMessage.Content.ReadAsStringAsync().Result;
+                    tokenBased = JsonConvert.DeserializeObject<JObject>(resultMessage);
+                    Session["TokenNumber"] = tokenBased["access_token"].ToString();
+                }
+            }
+            return Content(tokenBased["access_token"].ToString());
         }
+        
+        /*public async Task<ActionResult> GetStudent()
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Clear();
+                client.BaseAddress = new Uri("/Token");
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "");
+            }
+        }*/
     }
 }
